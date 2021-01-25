173474efe1fd05fff0b396bcb88fed017d688d62
Charlotte Merzbacher <cjmerzbacher@gmail.com>
2021-01-25 07:37:22 -0800

    initial visualization

--- zk_parser.py
+++ zk_parser.py
@@ -3,7 +3,15 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 import networkx as nx
-import plotly.graph_objects as go
+
+from bokeh.io import output_notebook, show, save
+from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine
+from bokeh.plotting import figure
+from bokeh.plotting import from_networkx
+from bokeh.palettes import Blues8, Reds8, Purples8, Oranges8, Viridis8, Spectral8
+from bokeh.transform import linear_cmap
+from networkx.algorithms import community
+
 
 mypath = '..'
 filelist = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -79,4 +87,37 @@ G = nx.Graph()
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
 
+#calculate node degrees
+degrees = dict(nx.degree(G))
+nx.set_node_attributes(G, name='degree', values=degrees)
+
 #plot graph
+title = 'Zettelkasten'
+
+#Establish which categories will appear when hovering over each node
+HOVER_TOOLTIPS = [("ID", "@index"), ("Degree", "@degree")]
+
+#Create a plot — set dimensions, toolbar, and title
+plot = figure(tooltips = HOVER_TOOLTIPS,
+              tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
+            x_range=Range1d(-10.1, 10.1), y_range=Range1d(-10.1, 10.1), title=title)
+
+#Create a network graph object with spring layout
+network_graph = from_networkx(G, nx.spring_layout, scale=500, center=(0, 0))
+
+#Set node size and color
+network_graph.node_renderer.glyph = Circle(size=15, fill_color='skyblue')
+
+#Set edge opacity and width
+network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
+
+#Add network graph to the plot
+plot.renderers.append(network_graph)
+
+show(plot)
+
+#Color if tag or non-tag
+#selective highlighting
+#Size based on degree
+#https://melaniewalsh.github.io/Intro-Cultural-Analytics/Network-Analysis/Making-Network-Viz-with-Bokeh.html
+
